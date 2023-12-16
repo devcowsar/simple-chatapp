274cff1c77d040eba57f893450249208e84d8cc7
devcowsar <kawsarcse836@gmail.com>
2023-12-16 13:55:49 -0800

    feat: client

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import io from "socket.io-client";
+export default class App extends Component {
+  componentDidMount() {
+    const socket = io.connect("/");
+  }
+  render() {
+    return <div>App</div>;
+  }
+}
