6af7435304e1bd27db902d65623b5298994cc570
devcowsar <kawsarcse836@gmail.com>
2023-12-17 00:03:23 -0800

    fix: serverandclient

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import io from "socket.io-client";
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      msg: "",
+    };
+  }
   componentDidMount() {
     const socket = io.connect("/");
+    socket.on("msg", (data) => {
+      this.setState({ msg: data });
+    });
   }
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <h1>{this.state.msg}</h1>
+      </div>
+    );
   }
 }
--- server.js
+++ server.js
@@ -5,9 +5,24 @@ const expressSever = http.createServer(app);
 const { Server } = require("socket.io");
 
 const io = new Server(expressSever);
+const path = require("path");
+
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.get("/express-server", (req, res) => {
+  res.end("this is my backend");
+});
 
 io.on("connection", (socket) => {
   console.log("new user connected");
+
+  setTimeout(() => {
+    socket.emit("msg", "this is msg from server");
+  }, 5000);
 
   socket.on("disconnect", () => {
     console.log("user Disconected");
